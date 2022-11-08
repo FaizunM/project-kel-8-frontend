407e068d69d3eaec740c677f3deacc6aba6a8ed0
FaizunM <muhamad.faizun.musthofa@gmail,com>
2022-11-08 16:11:12 +0700

    Menambahkan fitur search

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,26 @@ import Navigation from "./Navigation";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-[#f5f5f5]">
+    <div className="w-screen h-screen bg-white">
       <Navigation />
+      <div className="w-full h-auto">
+        <div className="w-full h-auto p-4">
+          <form action="">
+            <div className="flex">
+              <input
+                type="search"
+                className="w-full h-12 border-2 border-[rgba(0,0,0,0.1)] px-4 border-r-[rgba(0,0,0,0)] rounded-lg rounded-r-none outline-none focus:border-[rgba(0,0,0,0.25)] font-medium transition-all duration-150 ease-in-out"
+                name="s"
+                id="search"
+                placeholder="Cari nama peserta ujian"
+              />
+              <div className="w-14 h-12 border-2 border-[rgba(0,0,0,0.1)] border-l-0 rounded-lg rounded-l-none flex items-center justify-center text-[rgba(0,0,0,0.25)] hover:bg-[rgba(0,0,0,0.075)]">
+                <i className="fa-solid fa-search"></i>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 export default function Navigation() {
   return (
-    <div className="w-full h-[64px] bg-white flex items-center">
+    <div className="w-full h-[64px] bg-white flex items-center shadow-sm">
       <div className="w-auto h-auto px-4">App Name</div>
     </div>
   );
