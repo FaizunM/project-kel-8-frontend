577a8c076403616fc8527c79c68d8dce900ee622
FaizunM <muhamad.faizun.musthofa@gmail,com>
2022-11-09 13:58:16 +0700

    Menambahkan sidemenu di dashboard

--- src/components/admin/Dashboard.js
+++ src/components/admin/Dashboard.js
@@ -3,8 +3,97 @@ import Home from "./Home";
 
 export default function Dashboard() {
   return (
-    <Routes>
-      <Route index element={<Home />} />
-    </Routes>
+    <div className="w-full h-screen">
+      <div className="flex">
+        <div className="w-[300px] h-auto flex bg-white border-[1px] border-[rgba(0,0,0,0.075)] flex-col">
+          <div className="w-full h-12 flex items-center px-4">
+            <div className="w-auto h-auto font-medium">
+              SMK Negeri 2 Salatiga
+            </div>
+          </div>
+          <div className="w-full h-auto p-4">
+            <div className="w-full h-12 rounded-lg flex items-center hover:bg-[rgba(0,0,0,0.025)]">
+              <div className="w-12 h-12 flex items-center justify-center">
+                <i className="fa-solid fa-gauge"></i>
+              </div>
+              <div className="w-fit h-auto text-sm font-medium">
+                Dashboard
+              </div>
+            </div>
+            <div className="w-full h-12 rounded-lg flex items-center hover:bg-[rgba(0,0,0,0.025)]">
+              <div className="w-12 h-12 flex items-center justify-center">
+                <i className="fa-regular fa-user-circle"></i>
+              </div>
+              <div className="w-fit h-auto text-sm font-medium">
+                Profile Saya
+              </div>
+            </div>
+            <div className="w-full h-12 rounded-lg flex items-center hover:bg-[rgba(0,0,0,0.025)]">
+              <div className="w-12 h-12 flex items-center justify-center">
+                <i class="fa-solid fa-users"></i>
+              </div>
+              <div className="w-fit h-auto text-sm font-medium">
+                Data Diri Siswa
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <div className="w-full h-12 rounded-lg flex items-center hover:bg-[rgba(0,0,0,0.025)]">
+                <div className="w-12 h-12 flex items-center justify-center">
+                  <i className="fa-solid fa-box"></i>
+                </div>
+                <div className="w-fit h-auto text-sm font-medium">
+                  Data Master
+                </div>
+                <div className="w-fit h-auto ml-auto p-4 text-xs">
+                  <i className="fa-solid fa-chevron-down"></i>
+                </div>
+              </div>
+              <div className="hidden flex-col">
+                <div className="w-full h-12 rounded-lg flex items-center hover:bg-[rgba(0,0,0,0.025)]">
+                  <div className="w-12 h-12 flex items-center justify-center"></div>
+                  <div className="w-fit h-auto text-sm font-medium">
+                    Daftar Kelas
+                  </div>
+                </div>
+                <div className="w-full h-12 rounded-lg flex items-center hover:bg-[rgba(0,0,0,0.025)]">
+                  <div className="w-12 h-12 flex items-center justify-center"></div>
+                  <div className="w-fit h-auto text-sm font-medium">
+                    Daftar Sekolah
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full h-12 rounded-lg flex items-center hover:bg-[rgba(0,0,0,0.025)]">
+              <div className="w-12 h-12 flex items-center justify-center">
+                <i className="fa-solid fa-circle-info"></i>
+              </div>
+              <div className="w-fit h-auto text-sm font-medium">About</div>
+            </div>
+          </div>
+          <div className="w-full h-auto flex p-4 mt-auto">
+            <div className="w-auto h-auto flex items-center gap-3">
+              <div className="w-10 h-10 flex items-center justify-center overflow-hidden rounded-full">
+                <img
+                  src="https://onwaleed.sirv.com/Image/1146938-min.jpg?thumbnail=512"
+                  alt=""
+                  className="w-10 h-10"
+                />
+              </div>
+              <div className="w-[108px] overflow-hidden text-ellipsis whitespace-nowrap h-auto text-sm font-medium ">
+                Muhamad Faizun Musthofa
+              </div>
+            </div>
+            <div className="w-10 h-10 flex items-center justify-center bg-black ml-auto rounded-lg text-white hover:bg-[rgba(0,0,0,0.05)] hover:text-black transition-all duration-150 ease-in-out">
+              <i className="fa-solid fa-arrow-right-from-bracket"></i>
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-auto">
+          <Routes>
+            <Route index element={<Home />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/admin/Home.js
+++ src/components/admin/Home.js
@@ -1,3 +1,25 @@
 export default function Home() {
-  return <div className="w-full h-screen">Dashboard</div>;
+  return (
+    <div className="w-full h-screen px-16 py-16">
+      <h1 className="text-3xl font-semibold">Dashboard</h1>
+      <div className="w-full h-auto mt-16">
+        <div className="w-full h-auto grid grid-cols-3 items-center gap-3">
+          <div className="w-full h-auto p-4 bg-red-500 rounded-lg text-white">
+            <h3 className="text-xl font-semibold text-gray-100">
+              Jumlah Peserta
+            </h3>
+            <div className="w-auto h-auto text-3xl font-bold mt-4">
+              2.000 Siswa
+            </div>
+          </div>
+          <div className="w-full h-auto p-4 bg-green-500 rounded-lg text-white">
+            <h3 className="text-xl font-semibold text-gray-100">
+              Persentase lulus
+            </h3>
+            <div className="w-auto h-auto text-3xl font-bold mt-4">97.5%</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
