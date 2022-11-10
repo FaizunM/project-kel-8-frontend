8f43078d57d130511033e275807be9f0221793ed
FaizunM <muhamad.faizun.musthofa@gmail,com>
2022-11-10 19:38:35 +0700

    Menambahkan fitur tambah jurusan

--- src/components/admin/MajorList.js
+++ src/components/admin/MajorList.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
+import MJAddBox from "./MJAddBox";
+
 export default function MajorList() {
+  const [MJAddBoxState, setMJAddBoxState] = useState(false);
+
+  const openMJAddBox = () => {
+    setMJAddBoxState(true);
+  };
+
+  const closeMJAddBox = () => {
+    setMJAddBoxState(false);
+  };
+
   return (
     <div className="w-full h-screen px-16 py-16 overflow-y-scroll">
       <h1 className="text-3xl font-semibold">Daftar Jurusan</h1>
       <div className="w-full h-auto mt-16">
-        <div className="w-fit h-auto p-4 py-2 bg-white border-[1px] border-[rgba(0,0,0,0.075)] rounded-lg hover:shadow-default hover:border-[rgba(0,0,0,0.0)] transition-all duration-150 ease-in-out hover:bg-blue-300 hover:text-white">
+        <div
+          className="w-fit h-auto p-4 py-2 bg-white border-[1px] border-[rgba(0,0,0,0.075)] rounded-lg hover:shadow-default hover:border-[rgba(0,0,0,0.0)] transition-all duration-150 ease-in-out hover:bg-blue-300 hover:text-white"
+          onClick={openMJAddBox}
+        >
           Tambah
         </div>
         <div className="w-full h-full grid grid-cols-3 gap-3 mt-8">
@@ -25,6 +41,7 @@ export default function MajorList() {
           </div>
         </div>
       </div>
+      {MJAddBoxState ? <MJAddBox close={closeMJAddBox} /> : ""}
     </div>
   );
 }
