dea31f9b32ca1d1c3300bd48cc264cba41c4f1ae
FaizunM <muhamad.faizun.musthofa@gmail,com>
2022-11-10 19:08:27 +0700

    Menambahkan fitur tambah data pengguna

--- src/components/admin/StudentData.js
+++ src/components/admin/StudentData.js
@@ -1,9 +1,13 @@
+import SDAddBox from "./SDAddBox";
+
 export default function StudentData() {
   return (
     <div className="w-full h-screen px-16 py-16 overflow-y-scroll">
       <h1 className="text-3xl font-semibold">Data diri siswa</h1>
       <div className="w-full h-auto mt-16">
-        <div className="w-fit h-auto p-4 py-2 bg-white border-[1px] border-[rgba(0,0,0,0.075)] rounded-lg hover:shadow-default hover:border-[rgba(0,0,0,0.0)] transition-all duration-150 ease-in-out hover:bg-blue-300 hover:text-white">Tambah</div>
+        <div className="w-fit h-auto p-4 py-2 bg-white border-[1px] border-[rgba(0,0,0,0.075)] rounded-lg hover:shadow-default hover:border-[rgba(0,0,0,0.0)] transition-all duration-150 ease-in-out hover:bg-blue-300 hover:text-white">
+          Tambah
+        </div>
         <div className="w-full h-full grid grid-cols-3 gap-3 mt-8">
           <div className="w-full h-auto p-4 bg-white border-[1px] border-[rgba(0,0,0,0.075)] rounded-lg hover:shadow-default hover:border-0 transition-all duration-150 ease-in-out">
             <div className="w-full h-auto flex">
@@ -31,6 +35,7 @@ export default function StudentData() {
           </div>
         </div>
       </div>
+      <SDAddBox />
     </div>
   );
 }
